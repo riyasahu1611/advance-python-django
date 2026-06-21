493db2f7180da92f22e71c0be5d53267be6d6b5c
riyasahu1611 <riyasahu1611@gmail.com>
2026-06-21 15:35:04 +0530

    21-June-2026

--- sos-project/sos/ors/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrsConfig(AppConfig):
-    name = 'ors'
--- sos-project/sos/ors/utility/data_validator.py
+++
@@ -1,7 +0,0 @@
-class DataValidator:
-
-    def is_null(val):
-        if (val == ''):
-            return True
-        else:
-            False
